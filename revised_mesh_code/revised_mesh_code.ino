2123365db1044b95445dbbc7354e74842ec55650
Bhaswanth Ayapilla <bhaswanthayapilla@gmail.com>
2022-01-09 14:24:28 +0530

    add revised code update

--- revised_mesh_code/revised_mesh_code.ino
+++ revised_mesh_code/revised_mesh_code.ino
@@ -65,11 +65,14 @@ void loop() {
 }
 
 
-void mathAlgo(int n1, int n2)
+void mathAlgo(int n1, int n2)  // Dijkstra's algorithm
 {
   int MAX = 100;
   int n = 9;
-  int nextNode=0;
+  int i,j,u;
+  int cost[n][n], distance[n],pred[n];
+  int visited[n],count,mindistance,nextnode;
+  int nodeNext;
   
   int arr[n][n] = { { 0, 1, MAX, 1, 1, MAX, MAX, MAX, MAX },
                         { 1, 0, 1, 1, 1, 1, MAX, MAX, MAX },
@@ -82,26 +85,93 @@ void mathAlgo(int n1, int n2)
                         { MAX, MAX, MAX, MAX, 1, 1, MAX, 1, 0 }};
     
     
-    for(int k=0;k<n;k++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][k] == MAX || arr[k][j] == MAX)
-                    continue;
-                else if(arr[i][k] + arr[k][j] < arr[i][j]){
-                    arr[i][j] = arr[i][k] + arr[k][j];
-                }
-            }
+  for(i=0;i<n;i++){
+      for(j=0;j<n;j++){
+        if(arr[i][j]==0){
+          cost[i][j] = MAX;
         }
+        else{
+          cost[i][j] = arr[i][j];
+        }
+      }
     }
-  
-  
-  if(arr[n1][n2]-arr[n1][n1]>1 || arr[n1][n1]-arr[n1][n2]>1){
-    nextNode = 4;
-  }
-  
-  if(arr[n1][n2]-arr[n1][n1]==1 || arr[n1][n1]-arr[n1][n2]==1){
-    nextNode = n2;
-  }
-  
-  return nextNode;      
+    
+    for(i=0;i<n;i++)
+    {
+      distance[i] = cost[n1][i];
+      pred[i] = n1;
+      visited[i] = 0;
+    }
+    distance[n1] = 0;
+    visited[n1] = 1;
+    count = 1;
+    
+    while(count<=n+5)
+    {
+      mindistance = MAX;
+      
+      for(i=0;i<n;i++)
+      {
+        if(distance[i]<mindistance && !visited[i])
+        {
+          mindistance = distance[i];
+          nextnode = i;
+        }
+      }
+      
+      visited[nextnode] = 1;
+      for(i=0;i<n;i++)
+      {
+        if(!visited[i])
+        {
+          if(mindistance + cost[nextnode][i] < distance[i]){
+            distance[i] = mindistance + cost[nextnode][i];
+            pred[i] = nextnode;
+          }
+          count++;
+        }
+      }
+    }
+
+    // printing all node distances and paths
+//    for(i=0;i<n;i++)
+//    {
+//      if(i!=n1){
+//        Serial.println("");
+//        Serial.print("Distance of node");
+//        Serial.print(i);
+//        Serial.print("=");
+//        Serial.println(distance[i]);
+//        Serial.print("Path=");
+//        Serial.print(i);
+//        
+//        j=i;
+//        do
+//        {
+//          j=pred[j];
+//          Serial.print("<-");
+//          Serial.print(j);
+//        }while(j!=n1);
+//      }
+//    }        
+
+  for(i=0;i<n;i++)
+  {
+    if(i!=n1)
+    {         
+      Serial.println("");
+      
+      if(distance[i]==1)
+      {
+        nodeNext = i;
+      }
+      if(distance[i]==2)
+      {
+        nodeNext = pred[i];
+      }
+      
+    }
+  }   
+
+  return nodeNext;      
 }
